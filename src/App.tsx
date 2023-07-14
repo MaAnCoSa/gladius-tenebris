b1e7d95f8ef0e2c4484dbc9a1d1ef12047a07634
Manuel Cota <108767897+MaAnCoSa@users.noreply.github.com>
2023-07-13 22:25:19 -0700

    Added initial files
    Added the initial project.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import MainLock from './components/mainLock';
+
+function App() {
+  return (
+    <div className="App">
+      <MainLock />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mainLock.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MainLock = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: 'white',
+            backgroundColor: 'blue',
+            borderRadius: '10px',
+            margin: '50px'
+        }}>
+            Main Lock Component
+        </div>
+    );
+};
+
+export default MainLock;
